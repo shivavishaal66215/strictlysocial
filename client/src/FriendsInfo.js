bd7340f0f5c5b8dfd16a161651e54553024668b8
vishaal <shivavishaal25@gmail.com>
2021-10-29 21:49:20 +0530

    changed it to accept url parameter

--- client/src/FriendsInfo.js
+++ client/src/FriendsInfo.js
@@ -1,96 +1,83 @@
 import React, { Component } from "react";
 import axios from "axios";
+import avatar from "./images/undraw_male_avatar_323b.svg";
+import "./styles/Profile.css";
 const qs = require("qs");
-const fetchFriends = async () => {
-	try {
-		const res = await axios({
-			method: "get",
-			url: "/friends",
-			headers: {
-				"content-type": "application/x-www-form-urlencoded;charset=utf-8",
-			},
-			withCredentials: true,
-		});
-		return res.data;
-	} catch (e) {
-		console.log("cannot fetch friends");
-		return [];
-	}
-};
 
 export default class FriendsInfo extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			friends: [],
-			curFriend: {
-				username: "",
-				email: "",
-				phone: "",
-			},
+			username: "",
+			email: "",
+			phone: "",
 		};
-
-		this.handleInfo = this.handleInfo.bind(this);
 	}
 
 	async componentDidMount() {
-		const friends = await fetchFriends();
-		this.setState(() => {
-			return { ...this.state, friends: friends };
-		});
-	}
-
-	async handleInfo(e) {
-		const friend = e.target.id;
+		let res;
+		console.log(this.props.match.params.id);
 		try {
-			//fetching the target's profile
-			const res = await axios({
+			res = await axios({
 				method: "post",
 				url: "/getUserProfile",
 				data: qs.stringify({
-					username: friend,
+					friend: this.props.match.params.id,
 				}),
 				headers: {
 					"content-type": "application/x-www-form-urlencoded;charset=utf-8",
 				},
 				withCredentials: true,
 			});
-
+			console.log(res);
 			this.setState(() => {
 				return {
 					...this.state,
-					curFriend: res.data,
+					username: res.data.username,
+					email: res.data.email,
+					phone: res.data.phone,
 				};
 			});
 		} catch (e) {
-			console.log("cannot fetch details now");
+			alert("invalid");
 		}
 	}
 
 	render() {
 		return (
-			<div>
-				<h1>Friends</h1>
-				<ul>
-					{this.state.friends.map((ele) => {
-						return (
-							<li key={ele}>
-								<span>{ele}</span>
-								<button onClick={this.handleInfo} id={ele}>
-									View Info
-								</button>
-							</li>
-						);
-					})}
-				</ul>
-				<div>
-					<h3>Username</h3>
-					<div>{this.state.curFriend.username}</div>
-					<h3>Phone</h3>
-					<div>{this.state.curFriend.phone}</div>
-					<h3>Email</h3>
-					<div>{this.state.curFriend.email}</div>
+			<div className="Profile">
+				<div className="Profile-banner">
+					<img className="Profile-avatar" src={avatar} />
+					<div className="Profile-banner-right">
+						<div className="module-heading">{this.state.username}</div>
+						<div className="Profile-sidebar">
+							<div className="Profile-sidebar-content">Explore</div>
+							<div className="Profile-sidebar-content">Friends</div>
+							<div className="Profile-sidebar-content">Interests</div>
+						</div>
+					</div>
+				</div>
+				<div className="Profile-content">
+					<div className="Profile-subcontent">
+						<div className="module-heading Profile-large-spacing">Email</div>
+						<div className="Profile-subcontent-content">
+							<div className="Profile-current Profile-small-spacing">
+								<div className="underlined">Current Email:</div>
+								<div className="Profile-current-value">{this.state.email}</div>
+							</div>
+						</div>
+					</div>
+
+					<div className="Profile-subcontent">
+						<div className="module-heading Profile-large-spacing">Phone</div>
+						<div className="Profile-subcontent-content">
+							<div className="Profile-current Profile-small-spacing">
+								<div className="underlined">Current Phone:</div>
+								<div className="Profile-current-value">{this.state.phone}</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
