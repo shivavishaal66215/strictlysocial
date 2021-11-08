2ff958e0d20aee33e268055b2443382088262868
vishaal <shivavishaal25@gmail.com>
2021-11-08 16:25:13 +0530

    core styles work fine

--- client/src/Explore.js
+++ client/src/Explore.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-
 import axios from "axios";
+import explore from "./images/undraw_Explore_re_8l4v.svg";
+import avatar from "./images/undraw_male_avatar_323b.svg";
+import "./styles/Explore.css";
 const qs = require("qs");
-/*
-TODO
-1. Section containing recommendations based on interests
-*/
 
 const fetchRecommendations = async () => {
 	try {
@@ -42,6 +40,16 @@ export default class Discovery extends Component {
 
 	async handleRefresh() {
 		const recommendations = await fetchRecommendations();
+		// let recommendations = [
+		// 	"vishaal",
+		// 	"vishaal",
+		// 	"vishaal",
+		// 	"vishaal",
+		// 	"vishaal",
+		// 	"vishaal",
+		// 	"vishaal",
+		// 	"vishaal",
+		// ];
 
 		this.setState(() => {
 			return { ...this.state, recommendations: recommendations };
@@ -87,21 +95,40 @@ export default class Discovery extends Component {
 
 	render() {
 		return (
-			<div>
-				<h1>Recommendations</h1>
-				<ul>
-					{this.state.recommendations.map((ele) => {
-						return (
-							<li key={ele}>
-								<span>{ele}</span>
-								<button onClick={this.handleAddFriend} id={ele}>
-									Add friend
-								</button>
-							</li>
-						);
-					})}
-				</ul>
-				<button onClick={this.handleRefresh}>Refresh</button>
+			<div className="Explore">
+				<div className="Explore-leftcontainer">
+					<div className="module-heading large-spacer-vertical">Explore</div>
+					<img className="Explore-img" src={explore} />
+					<div className="Explore-leftcontainer-content">
+						We've done a little bit of good old behind the scenes magic and we
+						found some people you might like.
+						<br />
+						<br />
+						Go on then, make new friends.
+					</div>
+					<div onClick={this.handleRefresh} className="Explore-refresh">
+						Refresh
+					</div>
+				</div>
+				<div className="Explore-rightcontainer">
+					<div className="Explore-recommendationscontainer">
+						{this.state.recommendations.map((ele) => {
+							return (
+								<div key={ele} className="Explore-recommendation">
+									<img className="Explore-avatar" src={avatar} />
+									<div>{ele}</div>
+									<div
+										onClick={this.handleAddFriend}
+										id={ele}
+										className="Explore-add"
+									>
+										Add
+									</div>
+								</div>
+							);
+						})}
+					</div>
+				</div>
 			</div>
 		);
 	}
