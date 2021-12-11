5044ddb5978ca7eea698c85e82cd4752542228ec
vishaal <shivavishaal25@gmail.com>
2021-12-11 21:26:57 +0530

    added route security

--- client/src/Explore.js
+++ client/src/Explore.js
@@ -5,6 +5,36 @@ import avatar from "./images/undraw_male_avatar_323b.svg";
 import "./styles/Explore.css";
 const qs = require("qs");
 
+const checkCorrectUsername = async () => {
+	const username = localStorage.getItem("username");
+
+	//checking if username is even set
+	if (username === null || username === undefined || username === "") {
+		return false;
+	}
+
+	try {
+		const res = await axios({
+			method: "get",
+			url: "/myProfile",
+			headers: {
+				"content-type": "application/x-www-form-urlencoded;charset=utf-8",
+			},
+			withCredentials: true,
+		});
+
+		//checking if session username matches current username
+		//if not, make the user re-login
+		if (res.data.username !== username) {
+			throw Error("wrong user");
+		}
+	} catch (e) {
+		return false;
+	}
+
+	return true;
+};
+
 const fetchRecommendations = async () => {
 	try {
 		const res = await axios({
@@ -79,6 +109,11 @@ export default class Discovery extends Component {
 	}
 
 	async componentDidMount() {
+		const result = await checkCorrectUsername();
+		if (!result) {
+			this.props.history.push("/login");
+			return;
+		}
 		await this.handleRefresh();
 	}
 
